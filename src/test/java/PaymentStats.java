28906675a3595375e83878d8e895d80108e3311d
e.nikolaev.stc <e.nikolaev.stc@innopolis.ru>
2019-12-06 20:53:39 +0300

    commit3

---
+++ src/test/java/PaymentStats.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+
+public class PaymentStats {
+    Date date;
+    float summ;
+    String uid;
+    String idPointofSale;
+
+    public PaymentStats(Date date, float summ, String uid, String idPointofSale) {
+        this.date = date;
+        this.summ = summ;
+        this.uid = uid;
+        this.idPointofSale = idPointofSale;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public float getSumm() {
+        return summ;
+    }
+
+    public void setSumm(float summ) {
+        this.summ = summ;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getIdPointofSale() {
+        return idPointofSale;
+    }
+
+    public void setIdPointofSale(String idPointofSale) {
+        this.idPointofSale = idPointofSale;
+    }
+}
--- src/test/java/statsTest.java
+++ src/test/java/statsTest.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Stream;
@@ -16,12 +17,15 @@ import java.util.stream.Stream;
 public class statsTest {
 
     private List<PaymentsOfPointSaleDTO> paymentsOfPointSaleDTO;
+    private  List<PaymentStats> paymentStatsList;
     private  File file;
     private InputStreamReader inputStreamReader;
 
 
     @BeforeEach
     public void init(){
+        paymentStatsList =  new ArrayList<>();
+        paymentsOfPointSaleDTO = new ArrayList<>();
         file = new File("e:/file1.txt");
         InputStream inputStream = null;
         try {
@@ -36,11 +40,29 @@ public class statsTest {
     public void ReadDataFromFile() throws IOException {
         Path path =  file.toPath();
         try (Stream<String> lineStream = Files.lines(path)) {
-            lineStream.forEach(this::initPaymentsOfPointSaleDTO);
-            Stream<PaymentsOfPointSaleDTO>  paymentsOfPointSaleDTOStream =  paymentsOfPointSaleDTO.stream();
+            lineStream.forEach(this::initPaymentsOfStat);
+            Stream<PaymentStats> statsStream = paymentStatsList.stream();
+            sortStremByData(statsStream);
         }
         catch (IOException ignored) {
         }
+    }
+
+    private void sortStremByData(Stream<PaymentStats> stream){
+        stream.sorted((e1, e2) -> e2.getDate().compareTo(e1.getDate()));
+
+    }
+
+    private void initPaymentsOfStat(String s) {
+        String[] str  = s.split(" ");
+        try {
+            Date date = new SimpleDateFormat("dd.MM.yyyy").parse(str[0]);
+            PaymentStats paymentStats = new PaymentStats(date,Float.valueOf(str[4]),str[3],str[2]);
+            paymentStatsList.add(paymentStats);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void initPaymentsOfPointSaleDTO(String s) {
